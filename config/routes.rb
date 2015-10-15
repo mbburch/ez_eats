c01633c729e296343ba15dfddf3bd6af20615899
Justin Pease <justinpease2@gmail.com>
2015-10-15 15:49:52 -0600

    built resources, models basic views, and controllers for some of the  major parts of the program

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
+  root 'welcome#index'
 
-  # You can have the root of your site routed with "root"
-  # root 'welcome#index'
-
+  resources :reviews
+  resources :blogs
+  resources :challenges 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 

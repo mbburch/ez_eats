e997274faad4158ebc215b22db06f7aac4f87ad9
MB Burch <marybethburch@gmail.com>
2015-10-16 15:32:45 -0600

    change test titles

--- test/features/user_can_go_to_different_pages_test.rb
+++ test/features/user_can_go_to_different_pages_test.rb
@@ -1,48 +1,28 @@
 require 'test_helper'
 
 class WelcomeControllerTest < ActionController::TestCase
-  test "user can go to the blog pags" do
+  test "user can go to the blog page" do
     visit '/'
     click_link 'Blog'
     assert_equal blogs_path, current_path
   end
 
-  test "user can go to the blog pags" do
-    visit '/'
-    click_link 'Blog'
-    assert_equal blogs_path, current_path
-  end
-
-  test "user can go to the blog pags" do
+  test "user can go to the reviews page" do
     visit '/'
     click_link 'Reviews'
     assert_equal reviews_path, current_path
   end
 
-  test "user can go to the blog pags" do
+  test "user can go to the food challenges page" do
     visit '/'
     click_link 'Food Challenges'
     assert_equal challenges_path, current_path
   end
 
-  test "user can go to the blog pags" do
+  test "user can go to the about page" do
     skip
     visit '/'
     click_link 'About'
-    assert_equal ???????, current_path
-  end
-
-  test "user can go to the blog pags" do
-    skip
-    visit '/'
-    click_link 'Login'
-    assert_equal 'session#create' #??????? , current_path
-  end
-
-  test "user can go to the blog pags" do
-    skip
-    visit '/'
-    click_link 'Create Account'
-    assert_equal , current_path
+    assert_equal about_path, current_path
   end
 end

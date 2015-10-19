3e94139fd10c272f9815e2f75393ef67dfc781a6
MB Burch <marybethburch@gmail.com>
2015-10-18 22:37:45 -0600

    add dropdowns to blog index. Functionality incomplete

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,7 +1,21 @@
 class Blog < ActiveRecord::Base
+  has_many :taggings
+  has_many :tags, through: :taggings
+
   default_scope -> { order(created_at: :desc) }
+
 
   def date
     created_at.strftime("%b %d, %Y")
+  end
+
+  def tag_list
+    tags.join(", ")
+  end
+
+  def tag_list=(tags_string)
+    tag_names = tags_string.split(",").collect{|s| s.strip.downcase}.uniq
+    new_or_found_tags = tag_names.collect { |name| Tag.find_or_create_by(name: name)}
+    self.tags = new_or_found_tags
   end
 end

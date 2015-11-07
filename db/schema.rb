76ca1066d3171aa167a2c8a973908bb527b5df63
MB Burch <marybethburch@gmail.com>
2015-11-06 17:34:12 -0700

    add about page

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151020201007) do
+ActiveRecord::Schema.define(version: 20151030140043) do
+
+  create_table "abouts", force: :cascade do |t|
+    t.string   "title"
+    t.string   "body"
+    t.string   "author"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "blogs", force: :cascade do |t|
     t.datetime "created_at", null: false
